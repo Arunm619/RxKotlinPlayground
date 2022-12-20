ef3e2e7af4ce37df22b7e6c33907002a2136aff1
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 22:29:46 +0530

    Subscribing and disposing
    - keep track of the disposable
    - dispose it if not already whenever we want

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,34 @@
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 fun main() {
-    val observable: Observable<Int> = Observable.range(1, 5)//1
-
-    observable.subscribeBy(onError = {
-        println("Error ${it.message}")
-    }, onNext = {
-        println("Next $it")
-    }, onComplete = {
-        println("Done")
-    })
-    val observer: Observer<Int> = object : Observer<Int> {
-        //3
-        override fun onComplete() {
-            println("All Completed")
+    runBlocking {
+        val observable: Observable<Long> =
+            Observable.interval(300, TimeUnit.MILLISECONDS)//1
+        val observer: Observer<Long> = object : Observer<Long> {
+            lateinit var disposable: Disposable//2
+            override fun onSubscribe(d: Disposable) {
+                disposable = d//3
+            }
+            override fun onNext(item: Long) {
+                println("Received $item")
+                if(item>=100 && !disposable.isDisposed) {//4
+                    disposable.dispose()//5
+                    println("Disposed")
+                }
+            }
+            override fun onError(e: Throwable) {
+                println("Error ${e.message}")
+            }
+            override fun onComplete() {
+                println("Complete")
+            }
         }
-
-        override fun onNext(item: Int) {
-            println("Next $item")
-        }
-
-        override fun onError(e: Throwable) {
-            println("Error Occurred ${e.message}")
-        }
-
-        override fun onSubscribe(d: Disposable) {
-            println("New Subscription ")
-        }
+        observable.subscribe(observer)
+        delay(15000)//6
     }
-    observable.subscribe(observer)
 }
