a82d0d78fc944378860a1108a8b258cc6d473bcd
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 17:36:55 +0530

    Building sequences - Fibonacci series

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,12 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
-
-suspend fun longRunningTsk(): Long {//(1)
-    val time = measureTimeMillis {//(2)
-        println("Please wait")
-        delay(2000)//(3)
-        println("Delay Over")
-    }
-    return time
-}
-
 fun main(args: Array<String>) {
-    runBlocking {
-        println("aSync GlobalScope")
-        var time = measureTimeMillis {
-            val a1 = GlobalScope.async {
-                delay(1000)
-                delay(1000)
-            }
-            a1.await()
-        }
-        println("Total time: $time")
-
+    var a = 0
+    var b = 1
+    print("$a, ")
+    print("$b, ")
+    for(i in 2..100) {
+        val c = a+b
+        print("$c, ")
+        a=b
+        b=c
     }
 }
