a7870496c18d3ff2e4f2eb1ce7681c132f77aca0
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 22:35:41 +0530

    Cold observables - Those Observables, which have this particular behavior, that is, emitting items from the   beginning for each subscription, are called Cold Observable. To be more specific, Cold   Observables start running upon subscriptions and Cold Observable starts pushing   items after subscribe gets called, and pushes the same sequence of items on each   subscription.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,20 @@
 import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
+import io.reactivex.rxkotlin.toObservable
 
 fun main() {
-    runBlocking {
-        val observable: Observable<Long> =
-            Observable.interval(300, TimeUnit.MILLISECONDS)//1
-        val observer: Observer<Long> = object : Observer<Long> {
-            lateinit var disposable: Disposable//2
-            override fun onSubscribe(d: Disposable) {
-                disposable = d//3
-            }
-            override fun onNext(item: Long) {
-                println("Received $item")
-                if(item>=100 && !disposable.isDisposed) {//4
-                    disposable.dispose()//5
-                    println("Disposed")
-                }
-            }
-            override fun onError(e: Throwable) {
-                println("Error ${e.message}")
-            }
-            override fun onComplete() {
-                println("Complete")
-            }
-        }
-        observable.subscribe(observer)
-        delay(15000)//6
-    }
+    val observable: Observable<String> = listOf("String 1", "String 2", "String 3", "String 4").toObservable()//1
+    observable.subscribe({//2
+        println("Received $it")
+    }, {
+        println("Error ${it.message}")
+    }, {
+        println("Done")
+    })
+    observable.subscribe({//3
+        println("Received $it")
+    }, {
+        println("Error ${it.message}")
+    }, {
+        println("Done")
+    })
 }
