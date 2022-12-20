d945d10d66bab0dc7e602d1b27e913f5b21031b0
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 16:18:47 +0530

    Inline functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,6 @@
-fun highOrderFunc(a: Int, validityCheckFunc: (a: Int) -> Boolean) {//(1)
-    if (validityCheckFunc(a)) {//(2)
-        println("a $a is Valid")
-    } else {
-        println("a $a is Invalid")
-    }
-}
-
-
-enum class Gender {
-    MAN, WOMAN
-}
-
-fun getCheckBasedOnGender(gender: Gender,weight:Int, validation: (Int) -> Boolean) {
-    if(validation(weight)) {
-        println("The weight is valid for $gender")
-    } else {
-        println("The weight is not valid for $gender")
-    }
-}
-
-fun Int.isEven() = this.mod(2) == 0
+inline fun doSomeStuff(a: Int = 0) = a + a * a
 fun main() {
-    getCheckBasedOnGender(Gender.MAN, 100) { weight: Int ->
-        weight > 200
+    for (i in 1..10) {
+        println("$i Output ${doSomeStuff(i)}")
     }
-
-    getCheckBasedOnGender(Gender.WOMAN,200) { weight: Int ->
-        weight > 100
-    }
-    highOrderFunc(12) { a: Int -> a.isEven() }//(3)
-    highOrderFunc(19) { a: Int -> a.isEven() }
 }
