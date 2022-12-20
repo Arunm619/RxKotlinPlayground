4b9de54d98f5abe4163dccfcdd43b33f970fe610
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 16:20:01 +0530

    Inline functions - Best suited for functions with function types.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
-inline fun doSomeStuff(a: Int = 0) = a + a * a
-fun main() {
-    for (i in 1..10) {
-        println("$i Output ${doSomeStuff(i)}")
+inline fun highOrderFuncInline(a: Int, validityCheckFunc: (a: Int) -> Boolean) {
+    if (validityCheckFunc(a)) {
+        println("a $a is Valid")
+    } else {
+        println("a $a is Invalid")
     }
+}
+
+fun Int.isEven() = this.mod(2) == 0
+fun main() {
+    highOrderFuncInline(12) { a: Int -> a.isEven() }
+    highOrderFuncInline(19) { a: Int -> a.isEven() }
 }
