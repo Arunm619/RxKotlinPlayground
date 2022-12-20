5d7a5bcdf8a57e31291b0d526629c0d33afd5bf5
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 22:39:31 +0530

    Cold vs Hot Observables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,3 +18,15 @@ fun main() {
         println("Done")
     })
 }
+
+/*
+Cold Observables are passive, they don't emit anything until subscribe is called. Hot
+Observables are contrary to Cold Observables; it doesn't need subscriptions to start
+emission. While you can compare Cold Observables to CD/DVD recordings, Hot
+Observables are like TV channels—they continue broadcasting (emitting) their content,
+irrespective of whether anyone is watching (Observing) it or not.
+Hot Observables resemble events more than data. The events may carry data with them,
+but there is a time-sensitive component where Observers that subscribed lately can miss
+out previously emitted data. They are specifically useful for UI events while working with
+Android/JavaFX/Swing. They are also very useful in resembling server requests.
+*/
