5680189f9155e7b78baac1950db0f1e02c019f91
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 13:59:03 +0530

    Added push based observable approach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
-fun main(args: Array<String>) {
-    val list:List<Any> = listOf("One", 2, "Three", "Four", 4.5,
-        "Five", 6.0f) // 1
-    val iterator = list.iterator() // 2
-    while (iterator.hasNext()) { // 3
-        println(iterator.next()) // Prints each element 4
-    }
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
+fun main() {
+    val list: List<Any> = listOf(
+        "One", 2, "Three", "Four", 4.5, "Five", 6.0f
+    ) // 1
+    val observable: Observable<Any> = list.toObservable()
+    observable.subscribeBy( // named arguments for
+            //lambda Subscribers
+            onNext = {
+                println(it)
+            }, onError = { it.printStackTrace() }, onComplete = { println("Done!") })
 }
