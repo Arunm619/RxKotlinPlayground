5680a8048e72911d9a0459f1bf2e9d4d705e4efb
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 15:56:14 +0530

    Pure functions : names and lambda functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
-import java.util.*
-
-fun main(args: Array<String>) {
-    val sum = { x: Int, y: Int -> x + y } // (1)
-    println("Sum ${sum(12, 14)}")// (2)
-    val anonymousMult = { x: Int -> (Random().nextInt(15) + 1) * x }
-    // (3)
-    println("random output ${anonymousMult(2)}")// (4)
+fun square(n:Int):Int {//(1)
+    return n*n
+}
+fun main() {
+    println("named pure func square = ${square(3)}")
+    val cube = {n:Int -> n*n*n}//(2)
+    println("lambda pure func qube = ${cube(3)}")
 }
