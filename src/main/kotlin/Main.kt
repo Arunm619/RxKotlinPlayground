cc03f5814d1e1962d6c9a890b2e17476f70ddc0b
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 22:23:37 +0530

    when we connect Observable to Observer, it looks for these four methods in Observer and calls them. So, the following is a short description of the four methods: - onNext: Observable calls this method of Observer to pass each of the items one by one. - onComplete: When Observable wants to denote, it's done with passing items to the onNext method, and it calls the onComplete method of Observer. Observables, Observers, and Subjects - onError: When Observable faces any error, it calls the onError method to deal with the error if defined in the Observer, otherwise, it throws the exception. - onSubscribe: This method is called whenever a new Observable subscribes to the Observer.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,35 @@
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
+import io.reactivex.rxkotlin.subscribeBy
 
-fun main(args: Array<String>) {
-    val observer: Observer<Any> = object : Observer<Any> {
+fun main() {
+    val observable: Observable<Int> = Observable.range(1, 5)//1
+
+    observable.subscribeBy(onError = {
+        println("Error ${it.message}")
+    }, onNext = {
+        println("Next $it")
+    }, onComplete = {
+        println("Done")
+    })
+    val observer: Observer<Int> = object : Observer<Int> {
+        //3
         override fun onComplete() {
             println("All Completed")
         }
 
-        override fun onNext(item: Any) {
+        override fun onNext(item: Int) {
             println("Next $item")
         }
 
         override fun onError(e: Throwable) {
-            println("Error Occured ${e.message}")
+            println("Error Occurred ${e.message}")
         }
 
         override fun onSubscribe(d: Disposable) {
-            println("New Subscription $d")
+            println("New Subscription ")
         }
-    }//Create Observer
-    Observable.range(1, 10).subscribe(observer)//(1)
-    Observable.empty<String>().subscribe(observer)//(2)
-    runBlocking {
-        //doesn't complete forever, sends number out every x period.
-        //Observable.interval(), emits numbers
-        //sequentially starting from 0, after each specified interval. It will continue emitting until you
-        //unsubscribe and until the program runs
-        Observable.interval(300, TimeUnit.MILLISECONDS).subscribe(observer)//(3)
-        delay(900)
-
-
-        //simply completes after delay 400.
-        //Observable.timer(), will emit only once with 0 after the specified time elapsed.
-        Observable.timer(400, TimeUnit.MILLISECONDS).subscribe(observer)//(4)
-        delay(9000)
     }
+    observable.subscribe(observer)
 }
