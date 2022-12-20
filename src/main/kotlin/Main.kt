ce70fb0dca68f95e991cf35249df10319fd105f9
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 22:42:53 +0530

    The main purpose of ConnectableObservable is for Observables with multiple subscriptions to connect all subscriptions of an Observable together so that they can react to a single push; contrary to Cold Observables that repeats operations for doing the push, and pushes separately for each subscription, thus repeating the cycle. ConnectableObservable connects all subscriptions (Observers) called before the connect method and relays a single push to all Observers, Observers then react to/process that push.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,11 @@
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
 fun main() {
-    val observable: Observable<String> = listOf("String 1", "String 2", "String 3", "String 4").toObservable()//1
-    observable.subscribe({//2
-        println("Received $it")
-    }, {
-        println("Error ${it.message}")
-    }, {
-        println("Done")
-    })
-    observable.subscribe({//3
-        println("Received $it")
-    }, {
-        println("Error ${it.message}")
-    }, {
-        println("Done")
-    })
+    val connectableObservable = listOf("String 1","String 2","String 3","String 4","String 5").toObservable()
+    .publish()//1
+    connectableObservable.subscribe { println("Subscription 1: $it") }//2
+    connectableObservable.map(String::reversed)//3
+        .subscribe { println("Subscription 2 $it") }//4
+    connectableObservable.connect()//5
+    connectableObservable.subscribe { println("Subscription 3: $it") }//6 //Will not receive emissions
 }
-
-/*
-Cold Observables are passive, they don't emit anything until subscribe is called. Hot
-Observables are contrary to Cold Observables; it doesn't need subscriptions to start
-emission. While you can compare Cold Observables to CD/DVD recordings, Hot
-Observables are like TV channels—they continue broadcasting (emitting) their content,
-irrespective of whether anyone is watching (Observing) it or not.
-Hot Observables resemble events more than data. The events may carry data with them,
-but there is a time-sensitive component where Observers that subscribed lately can miss
-out previously emitted data. They are specifically useful for UI events while working with
-Android/JavaFX/Swing. They are also very useful in resembling server requests.
-*/
