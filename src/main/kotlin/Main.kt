57fbdcbb2387cc28c3ec7a8ff47c4e35471286e3
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 21:58:28 +0530

    Observable factory creation methods - Observable.just Just Like that!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,45 @@
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import java.util.concurrent.Callable
-import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
 
 fun main() {
-    val observer: Observer<String> = object : Observer<String> {
+    val observer: Observer<Any> = object : Observer<Any> {
         override fun onComplete() {
             println("All Completed")
         }
-        override fun onNext(item: String) {
+
+        override fun onNext(item: Any) {
             println("Next $item")
         }
+
         override fun onError(e: Throwable) {
             println("Error Occured ${e.message}")
         }
+
         override fun onSubscribe(d: Disposable) {
-            println("New Subscription $d")
+            println("New Subscription ")
         }
     }//Create Observer
-    val list = listOf("String 1","String 2","String 3","String 4")
-    val observableFromIterable: Observable<String> =
-        Observable.fromIterable(list)//1
-    observableFromIterable.subscribe(observer)
-    val callable = object : Callable<String> {
-        override fun call(): String {
-            return "From Callable"
-        }
-    }
-    val observableFromCallable:Observable<String> =
-        Observable.fromCallable(callable)//2
-    observableFromCallable.subscribe(observer)
-    val future:Future<String> = object : Future<String> {
-        override fun get(): String = "Hello From Future"
-        override fun get(timeout: Long, unit: TimeUnit?): String =
-            "Hello From Future"
-        override fun isDone(): Boolean = true
-        override fun isCancelled(): Boolean = false
-        override fun cancel(mayInterruptIfRunning: Boolean):Boolean = false
-    }
-    val observableFromFuture: Observable<String> =
-        Observable.fromFuture(future)//3
-    observableFromFuture.subscribe(observer)
+    Observable.just("A String").subscribe(observer)
+    Observable.just(42).subscribe(observer)
+    Observable.just(
+        listOf(
+            "String 1", "String 2", "String 3", "String 4"
+        )
+    ).subscribe(observer)
+    Observable.just(
+        mapOf(
+            Pair("Key 1", "Value 1"), Pair("Key 2", "Value 2"), Pair(
+                "Key 3", "Value 3"
+            )
+        )
+    ).subscribe(observer)
+    Observable.just(
+        arrayListOf(
+            1, 2, 3, 4, 5, 6
+        )
+    ).subscribe(observer)
+    Observable.just(
+        "String 1", "String 2", "String 3","String 1", "String 2", "String 3",
+    ).subscribe(observer)//1
 }
