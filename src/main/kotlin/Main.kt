be71397ea2fe9fd3deffd50edd1c2f79b04f5631
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 16:07:44 +0530

    Higher order functions - own example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,28 @@ fun highOrderFunc(a: Int, validityCheckFunc: (a: Int) -> Boolean) {//(1)
     }
 }
 
+
+enum class Gender {
+    MAN, WOMAN
+}
+
+fun getCheckBasedOnGender(gender: Gender,weight:Int, validation: (Int) -> Boolean) {
+    if(validation(weight)) {
+        println("The weight is valid for $gender")
+    } else {
+        println("The weight is not valid for $gender")
+    }
+}
+
 fun Int.isEven() = this.mod(2) == 0
 fun main() {
+    getCheckBasedOnGender(Gender.MAN, 100) { weight: Int ->
+        weight > 200
+    }
+
+    getCheckBasedOnGender(Gender.WOMAN,200) { weight: Int ->
+        weight > 100
+    }
     highOrderFunc(12) { a: Int -> a.isEven() }//(3)
     highOrderFunc(19) { a: Int -> a.isEven() }
 }
