97e60ebd29a3629d2524aea217d978328973dea7
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 17:52:41 +0530

    Monad is a structure that creates a new type by encapsulating a value and adding some extra functionalities - Maybe monad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
-fun main(args: Array<String>) {
-    runner()
+import io.reactivex.Maybe
+import io.reactivex.rxkotlin.subscribeBy
+
+fun main() {
+    val maybeValue: Maybe<Int> = Maybe.just(14)//1
+    maybeValue.subscribeBy(//2
+        onComplete = {println("Completed Empty")},
+        onError = {println("Error $it")},
+        onSuccess = { println("Completed with value $it")}
+    )
+    val maybeEmpty:Maybe<Int> = Maybe.empty()//3
+    maybeEmpty.subscribeBy(
+        onComplete = {println("Completed Empty")},
+        onError = {println("Error $it")},
+        onSuccess = { println("Completed with value $it")}
+    )
 }
