4fab5118990cd568c173a4b4080089e8af4fea43
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-20 15:54:16 +0530

    Lambdas - Anonymous functions / nameless functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,88 +1,9 @@
-import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
 import java.util.*
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
-fun main() {
-    println("Initial Out put with a = 15, b = 10")
-    val calculator = ReactiveCalculator(15, 10)
-    println(
-        "Enter a = <number> or b = <number> in separate lines \n exit to exit the program "
-    )
-    var line: String?
-    do {
-        line = readlnOrNull()
-        calculator.handleInput(line)
-    } while (line != null && !line.lowercase(Locale.getDefault()).contains("exit"))
-}
-
-class ReactiveCalculator(a: Int, b: Int) {
-    private val subjectAdd: Subject<Pair<Int, Int>> = PublishSubject.create()
-    private val subjectSub: Subject<Pair<Int, Int>> = PublishSubject.create()
-    private val subjectMult: Subject<Pair<Int, Int>> = PublishSubject.create()
-    private val subjectDiv: Subject<Pair<Int, Int>> = PublishSubject.create()
-    private val subjectCalc: Subject<ReactiveCalculator> = PublishSubject.create()
-    private var nums: Pair<Int, Int> = Pair(0, 0)
-    init {
-        nums = Pair(a, b)
-
-        subjectAdd.map { it.first + it.second }.subscribe { println("Add = $it") }
-        subjectSub.map { it.first - it.second }.subscribe { println("Substract = $it") }
-        subjectMult.map { it.first * it.second }.subscribe { println("Multiply = $it") }
-        subjectDiv.map { it.first / (it.second * 1.0) }.subscribe { println("Divide = $it") }
-        subjectCalc.subscribe {
-            with(it) {
-                println("a=${this.nums.first}, b=${this.nums.second}")
-                calculateAddition()
-                calculateSubstraction()
-                calculateMultiplication()
-                calculateDivision()
-            }
-        }
-        subjectCalc.onNext(this)
-    }
-
-    private fun calculateAddition() {
-        subjectAdd.onNext(nums)
-    }
-
-    private fun calculateSubstraction() {
-        subjectSub.onNext(nums)
-    }
-
-    private fun calculateMultiplication() {
-        subjectMult.onNext(nums)
-    }
-
-    private fun calculateDivision() {
-        subjectDiv.onNext(nums)
-    }
-
-    private fun modifyNumbers(a: Int = nums.first, b: Int = nums.second) {
-        nums = Pair(a, b)
-        subjectCalc.onNext(this)
-    }
-
-    fun handleInput(inputLine: String?) {
-        if (!inputLine.equals("exit")) {
-            val pattern: Pattern = Pattern.compile("([a|b])(?:\\s)?=(?:\\s)?(\\d*)")
-            var a: Int? = null
-            var b: Int? = null
-            val matcher: Matcher = pattern.matcher(inputLine)
-            if (matcher.matches() && matcher.group(1) != null && matcher.group(2) != null) {
-                if (matcher.group(1).lowercase(Locale.getDefault()) == "a") {
-                    a = matcher.group(2).toInt()
-                } else if (matcher.group(1).lowercase(Locale.getDefault()) == "b") {
-                    b = matcher.group(2).toInt()
-                }
-            }
-            when {
-                a != null && b != null -> modifyNumbers(a, b)
-                a != null -> modifyNumbers(a = a)
-                b != null -> modifyNumbers(b = b)
-                else -> println("Invalid Input")
-            }
-        }
-    }
+fun main(args: Array<String>) {
+    val sum = { x: Int, y: Int -> x + y } // (1)
+    println("Sum ${sum(12, 14)}")// (2)
+    val anonymousMult = { x: Int -> (Random().nextInt(15) + 1) * x }
+    // (3)
+    println("random output ${anonymousMult(2)}")// (4)
 }
