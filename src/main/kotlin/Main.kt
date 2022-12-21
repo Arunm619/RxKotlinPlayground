924c2eed96057b00bb4faf8618d2cf270ed37aba
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-21 14:30:13 +0530

    PublishSubject emits all subsequent values that it got at the time of subscription, whether it got the value via the onNext method or through another subscription. We have already seen the application of PublishSubject, and it is the most commonly used Subject variant.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
-import io.reactivex.Observable
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.subjects.AsyncSubject
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 /**
  * AsyncSubject
@@ -9,30 +8,7 @@ import io.reactivex.subjects.AsyncSubject
  * ReplaySubject
  * */
 fun main() {
-    asyncSubjectOnlyExample()
-}
-
-/**
- * AsyncSubject only emits the last value of the source observable (Observable it listens
- * on), and the last emission only. To say things more clearly, AsyncSubject will emit the last
- * value it got, and will emit it only one time.
- * */
-fun asyncSubject() {
-    val observable = Observable.just(1, 2, 3, 4)//1
-    val subject = AsyncSubject.create<Int>()//2
-    observable.subscribe(subject)//3
-    subject.subscribeBy(onNext = {//4
-        println("Received $it")
-    }, onError = {
-        it.printStackTrace()
-    }, onComplete = {
-        println("Complete")
-    })
-    subject.onComplete()//5
-}
-
-fun asyncSubjectOnlyExample() {
-    val subject = AsyncSubject.create<Int>()
+    val subject = PublishSubject.create < Int >()
     subject.onNext(1)
     subject.onNext(2)
     subject.onNext(3)
@@ -40,10 +16,10 @@ fun asyncSubjectOnlyExample() {
     subject.subscribe({
         //onNext
         println("S1 Received $it")
-    },{
+    }, {
         //onError
         it.printStackTrace()
-    },{
+    }, {
         //onComplete
         println("S1 Complete")
     })
@@ -51,13 +27,12 @@ fun asyncSubjectOnlyExample() {
     subject.subscribe({
         //onNext
         println("S2 Received $it")
-    },{
+    }, {
         //onError
         it.printStackTrace()
-    },{
+    }, {
         //onComplete
         println("S2 Complete")
     })
-    //have to trigger onComplete
     subject.onComplete()
 }
