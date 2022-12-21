28f79ff4b938f01efa3cc6849fbd0a49351a7f02
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-21 14:53:33 +0530

    Back Pressure Introduction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,21 @@
-import io.reactivex.subjects.ReplaySubject
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
-/**
- * AsyncSubject
- * PublishSubject
- * BehaviorSubject
- * ReplaySubject
- * */
 fun main() {
-    val subject = ReplaySubject.create < Int >()
-    subject.onNext(1)
-    subject.onNext(2)
-    subject.onNext(3)
-    subject.onNext(4)
-    subject.subscribe({
-        //onNext
-        println("S1 Received $it")
-    }, {
-        //onError
-        it.printStackTrace()
-    }, {
-        //onComplete
-        println("S1 Complete")
-    })
-    subject.onNext(5)
-    subject.subscribe({
-        //onNext
-        println("S2 Received $it")
-    }, {
-        //onError
-        it.printStackTrace()
-    }, {
-        //onComplete
-        println("S2 Complete")
-    })
-    subject.onNext(6)
-    subject.onComplete()
+    val observable = Observable.just(1,2,3,4,5,6,7,8,9)//(1)
+    val subject = BehaviorSubject.create<Int>()
+    subject//.observeOn(Schedulers.computation())//(2)
+        .subscribe {//(3)
+            println("Subs 1 Received $it")
+            runBlocking { delay(200) }//(4)
+        }
+    subject.observeOn(Schedulers.computation())//(5)
+        .subscribe {//(6)
+            println("Subs 2 Received $it")
+        }
+    observable.subscribe(subject)//(7)
+    runBlocking { delay(2000) }//(8)
 }
