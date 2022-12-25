be45cd22cd51bfc55edf693111019d5b607ce3bc
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:31:46 +0530

    The distinctUntilChange operator is slightly different. Instead of discarding all duplicate emissions, it discards only consecutive duplicate emissions, keeping the rest at its place. The distinct operator remembers each item until it receives onComplete, but the distinctUntilChanged operator remembers them only until it receives a new item.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 
-fun main() {
-    listOf(1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 3, 10)//(1)
+fun main(args: Array<String>) {
+    listOf(1,2,2,3,4,5,5,5,6,7,8,9,3,10)//(1)
         .toObservable()//(2)
-        .distinct()//(3)
+        .distinctUntilChanged()//(3)
         .subscribe { println("Received $it") }//(4)
 }
