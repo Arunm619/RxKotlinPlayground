ab9e4286a60f424a839b7bfeb812b9750477f3ae
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:50:23 +0530

    Error handling.
     the
    problem with the onError event is that the error is emitted to the downstream consumer
    chain, and the subscription is terminated instantly.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,36 @@
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.subscribeBy
 
-fun main() {
-    Observable.range(1,10)
-        .toList()
-        .subscribeBy { println(" $it") }
+fun main(args: Array<String>) {
+    Observable.just(1,2,3,5,6,7,"Errr",8,9,10)
+        .map { it.toIntOrError() }
+        .subscribeBy (
+            onNext = {
+                println("Next $it")
+            },
+            onError = {
+                println("Error $it")
+            }
+        )
 }
+
+fun Any?.toIntOrError() : Int {
+     if(this is Int) return this else throw Exception("WTF bro, pass an int.")
+}
+
 /**
- * Though it is not good practice, keeping some rare situations in mind, RxKotlin provides
- * you with operators that can listen to all the emissions and accumulate them to a collection
- * object.
- * The collection operators are basically a subset of the reducing operators.
- * Covered in later chapters.
+The program throws an exception in the map operator when the string Errr is emitted from
+the Observable. The exception was caught by the onError handler, but the Subscription
+doesn't get any further emissions.
+This may not be the desired behavior every time. Although we cannot pretend the error
+never happened and continue (we should not do this either), there should be a way to at
+least resubscribe or switch to an alternate source producer.
+ * */
+
+/**
+ * onErrorResumeNext( )
+ * onErrorReturn( )
+ * onExceptionResumeNext( )
+ * retry( )
+ * retryWhen()
  * */
