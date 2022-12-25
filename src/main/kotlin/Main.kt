24a896ed42c4b33435b25b3ea9696811adeeb7c1
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:39:38 +0530

    Accumulating data – scan operator
    The scan operator is a rolling aggregator; it emits incremental accumulation by adding
    previous emissions to it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,18 @@
+import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
-fun main(args: Array<String>) {
-
-    println("default with integer")
-    listOf(2, 6, 7, 1, 3, 4, 5, 8, 10, 9)
-        .toObservable()
-        .sorted()//(1)
+fun main() {
+    Observable.range(1, 10)
+        .scan { previousAccumulation: Int, newEmission: Int ->
+            previousAccumulation + newEmission
+        }//(1)
         .subscribe { println("Received $it") }
-
-
-    println("default with String")
-    listOf("alpha", "gamma", "beta", "theta")
-        .toObservable()
-        .sorted()//(2)
+    listOf("String 1", "String 2", "String 3", "String 4").toObservable().scan { previousAccumulation, newEmission ->
+            "$previousAccumulation $newEmission"
+        }//(2)
         .subscribe { println("Received $it") }
-
-    println("custom sortFunction with integer")
-    listOf(2, 6, 7, 1, 3, 4, 5, 8, 10, 9)
-        .toObservable()
-        .sorted { item1, item2 -> if (item1 > item2) -1 else 1 }//(3)
-        .subscribe { println("Received $it") }
-
-
-    println("custom sortFunction with custom class-object")
-    listOf(
-        MyItem1(2),
-        MyItem1(6),
-        MyItem1(7),
-        MyItem1(1),
-        MyItem1(3),
-        MyItem1(4),
-        MyItem1(5),
-        MyItem1(8),
-        MyItem1(10),
-        MyItem1(9)
-    ).toObservable().sorted { item1, item2 ->
-        if (item1.item < item2.item) -1 else 1
-    }//(4)
+    Observable.range(1, 5).scan { previousAccumulation, newEmission ->
+            previousAccumulation * 10 + newEmission
+        }//(3)
         .subscribe { println("Received $it") }
 }
-
-data class MyItem1(val item: Int)
