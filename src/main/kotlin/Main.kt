12f824e94f3ee9d7f3986231e2fa4ffd26f278c0
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 01:50:48 +0530

    Zipping emissions – zip operator
    , the zip operator accumulates emissions from multiple producers
    into a single emission. It also takes a function to apply on the emissions as the scan or
    reduce operator, but applies them to emissions from different producers.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,32 @@
 import io.reactivex.Observable
 
 fun main(args: Array<String>) {
-    println("startWith Iterator")
-    Observable.range(5,10)
-        .startWith(listOf(1,2,3,4))//(1)
-        .subscribe {
-            println("Received $it")
-        }
-    println("startWith another source Producer")
-    Observable.range(5,10)
-        .startWith(Observable.just(1,2,3,4))//(2)
-        .subscribe {
-            println("Received $it")
-        }
+    val observable1 = Observable.range(1,10)
+    val observable2 = Observable.range(11,10)
+    Observable.zip(observable1,observable2) { emissionO1, emissionO2 ->
+        emissionO1 + emissionO2
+    }.subscribe {
+        println("Received $it")
+    }
 }
 
 /**
-We can pass another source Observable or an Iterator instance to be prepended before
-the source Observable that the operator has subscribed to starts emitting.
-In the preceding program, on comment (1), we used the startWith operator and passed
-an Interator instance to it. The startWith operator internally converts the passed
-Iterator instance to an Observable instance (it'll convert it to a Flowable instance in
-case you're using Flowable). Here is the signature of the startWith operator:
-fun startWith(items: Iterable<T>): Observable<T> {
-return concatArray<T>(fromIterable<out T>(items), this)
-}
- * */
+In order to understand and use the zip operator better, you need to keep the following
+points about it in mind:
+- The zip operator works on each emission of the supplied producers. For
+example, if you pass three producers x, y, and z to the zip operator, it will
+accumulate the n
+th emission of x with the n
+th emission of y and z.
+- The zip operator waits for each of its producers to emit, before applying the
+function to them. For example, if you use Observable.interval as one of the
+producers in the zip operator, the zip operator will wait for each emission and
+will emit the accumulated values at the specified intervals as well.
+- If any of the producers notify onComplete or onError without emitting the item
+it was waiting for, then it'll discard all emissions afterwards, including that
+particular one from other producers as well. For example, if producer x emits 10
+items, producer y emits 11 items, and producer z emits 8 items, the zip operator
+will accumulate the first 8 emissions from all the producers and will discard all
+remaining emissions from producer x and y.
+
+ */
