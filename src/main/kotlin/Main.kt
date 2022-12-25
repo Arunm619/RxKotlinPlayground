afd3e4d0c1b0d538827df44d68f1b79057431402
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:57:58 +0530

    SwitchMap
    When the source Observable emits more than one item consecutively before the
     switchMap has emitted any of them, switchMap will take the last one and discard any emission that came in between

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,14 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    println("Without delay")
     Observable.range(1,10)
         .switchMap {
-            return@switchMap Observable.just(it)//(1)
-        }
-        .blockingSubscribe {
-            println("Received $it")
-        }
-    println("With delay")
-    Observable.range(1,15)
-        .switchMap {
             val randDelay = Random().nextInt(10)
-            return@switchMap Observable.just(it)
-                .also {
-                    //println("The value is ${it.blockingSingle()}")
-                }
-                .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)//(2)
+            if(it%3 == 0)
+                Observable.just(it)
+            else
+                Observable.just(it)
+                    .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)
         }
         .blockingSubscribe {
             println("Received $it")
@@ -27,7 +18,14 @@ fun main() {
 }
 
 /**
+ *  instead of adding delay to all the emissions, we emitted all the numbers
+ * divisible by 3 without delay, and added a delay to the rest.
+ * */
+
+/**
  * When the source Observable emits more than one item consecutively before the
  * switchMap has emitted any of them, switchMap will take the last one and discard any
  * emission that came in between
  * */
+
+// Check this: https://stackoverflow.com/a/38391535
