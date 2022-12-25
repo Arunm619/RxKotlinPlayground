97a67b1cc09e3f4d1068fea97371588a94cd5a20
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:04:42 +0530

    Merging Observables/Flowables – merge operator
     the merge operator merged two Observables and put the emissions
    of both the Observables in their order of emission.
    The merging operation, however, doesn't maintain the order specified; rather, it'll start
    listening to all the provided producers instantly and will fire emissions as soon as they are
    emitted from the source

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val observable1 = listOf("Kotlin", "Scala",
-        "Groovy").toObservable()
-    val observable2 = listOf("Python", "Java", "C++",
-        "C").toObservable()
+    val observable1 = Observable.interval(500,
+        TimeUnit.MILLISECONDS).map { "Observable 1 $it" }//(1)
+    val observable2 = Observable.interval(100,
+        TimeUnit.MILLISECONDS).map { "Observable 2 $it" }//(2)
     Observable
-        .merge(observable1,observable2)//(1)
+        .merge(observable1,observable2)
         .subscribe {
             println("Received $it")
         }
+    runBlocking { delay(1500) }
 }
 
 /**
