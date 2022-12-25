bfe637f996ca2a367419aa5661f7c822e6d7ec51
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 01:52:20 +0530

    The zipWith operator
    The instance version (that is, the copy of the function, which should be called with an
    instance rather than static) of the zip operator is zipWith, which can be called from the
    Observable instance itself. The only problem with this version is that you can pass only
    another source Observable. If you need to work with three or more Observable
    instances, you should rather consider using the zip operator instead of zipWith.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,15 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.toObservable
 
-fun main(args: Array<String>) {
+fun main() {
     val observable1 = Observable.range(1,10)
-    val observable2 = Observable.range(11,10)
-    Observable.zip(observable1,observable2) { emissionO1, emissionO2 ->
-        emissionO1 + emissionO2
-    }.subscribe {
-        println("Received $it")
-    }
+    val observable2 = listOf("String 1","String 2","String 3",
+        "String 4","String 5","String 6","String 7","String 8",
+        "String 9","String 10").toObservable()
+    observable1.zipWith(observable2) { e1: Int, e2: String ->
+        "$e2 $e1"
+    }//(1)
+        .subscribe {
+            println("Received $it")
+        }
 }
-
-/**
-In order to understand and use the zip operator better, you need to keep the following
-points about it in mind:
-- The zip operator works on each emission of the supplied producers. For
-example, if you pass three producers x, y, and z to the zip operator, it will
-accumulate the n
-th emission of x with the n
-th emission of y and z.
-- The zip operator waits for each of its producers to emit, before applying the
-function to them. For example, if you use Observable.interval as one of the
-producers in the zip operator, the zip operator will wait for each emission and
-will emit the accumulated values at the specified intervals as well.
-- If any of the producers notify onComplete or onError without emitting the item
-it was waiting for, then it'll discard all emissions afterwards, including that
-particular one from other producers as well. For example, if producer x emits 10
-items, producer y emits 11 items, and producer z emits 8 items, the zip operator
-will accumulate the first 8 emissions from all the producers and will discard all
-remaining emissions from producer x and y.
-
- */
