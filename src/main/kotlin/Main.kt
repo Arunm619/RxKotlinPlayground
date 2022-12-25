645d9b6e723b3d0927d7ae14d8791331987ad793
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:06:38 +0530

    The program is simple enough. We are creating two Observable instances, and then merging observable1 with observable2 with the mergeWith operator called on the observable1 instance.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,13 @@
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
 fun main(args: Array<String>) {
-    val observable1 = listOf("A", "B", "C").toObservable()
-    val observable2 = listOf("D", "E", "F", "G").toObservable()
-    val observable3 = listOf("I", "J", "K", "L").toObservable()
-    val observable4 = listOf("M", "N", "O", "P").toObservable()
-    val observable5 = listOf("Q", "R", "S", "T").toObservable()
-    val observable6 = listOf("U", "V", "W", "X").toObservable()
-    val observable7 = listOf("Y", "Z").toObservable()
-    Observable.mergeArray(observable1, observable2, observable3,
-        observable4, observable5, observable6, observable7)
+    val observable1 = listOf("Kotlin", "Scala",
+        "Groovy").toObservable()
+    val observable2 = listOf("Python", "Java", "C++",
+        "C").toObservable()
+    observable1
+        .mergeWith(observable2)
         .subscribe {
             println("Received $it")
         }
 }
-
-/**
- * The zipping operation will let you accumulate emissions, but what if you want to subscribe
- * to each emission by all the source producers? Say you have two different producers and
- * have the same set of actions to be applied when subscribing to them; there's no way to mix
- * imperative programming and reactive programming and repeatedly subscribe to both of
- * the producers separately with the same code. It'll also result in redundant code. So, what is
- * the solution here? You got it right; merging all the emissions of all the source producers
- * together and subscribing to them as a whole is the solution.
- *
- * */
