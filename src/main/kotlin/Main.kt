b70b0c2e89ab0f0c84c0f285134c58facea9a3bc
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:56:25 +0530

    The flatMap operator
    Where the map operator takes each emission and transforms them, the flatMap operator
    creates a new producer, applying the function you passed to each emission of the source
    producer.
    
    The output is similar to the previous one, but the logic is different. Instead of just returning
    the String, we are returning Observable with the desired String. Although, for this
    example, you seem to have no benefit using it, think of a situation when you need to derive
    multiple items from a single emission.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,11 @@
+import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
-fun main() {
-    val list = listOf<MyItemInherit>(
-        MyItemInherit(1),
-        MyItemInherit(2),
-        MyItemInherit(3),
-        MyItemInherit(4),
-        MyItemInherit(5),
-        MyItemInherit(6),
-        MyItemInherit(7),
-        MyItemInherit(8),
-        MyItemInherit(9),
-        MyItemInherit(10)
-    )//(1)
-    list.toObservable()//(2)
-        .map { it as MyItem}//(3)
-        .subscribe {
-            println(it)
-        }
-    println("cast")
-    list.toObservable().cast(MyItem::class.java)//(4)
-        .subscribe {
-            println(it)
-        }
-}
-
-open class MyItem(val id: Int) {
-    //(5)
-    override fun toString(): String {
-        return "[MyItem $id]"
-    }
-}
-
-class MyItemInherit(id: Int) : MyItem(id) {
-    //(6)
-    override fun toString(): String {
-        return "[MyItemInherit $id]"
+fun main(args: Array<String>) {
+    val observable = listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1).toObservable()
+    observable.flatMap { number ->
+        Observable.just("Transforming Int to String $number")
+    }.subscribe { item ->
+        println("Received $item")
     }
 }
