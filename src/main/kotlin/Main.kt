05e65df833af2da76409c747a15794a672ec4cb0
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 22:26:19 +0530

    Processor Processors are the counterparts for Subjects in Flowable. Every type of Subject has its counterpart as processor with backpressure support.
    The processor is waiting for all its Subscribers to complete before pushing the next
    emission.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,20 @@
+import io.reactivex.processors.PublishProcessor
 import io.reactivex.rxkotlin.toFlowable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    val connectableFlowable = listOf(
-        "String 1",
-        "String 2",
-        "String 3",
-        "String 4",
-        "String 5"
+    val flowable = listOf(
+        "String 1", "String 2", "String 3", "String 4", "String 5"
     ).toFlowable()//(1)
-        .publish()//(2)
-    connectableFlowable.subscribe {
+    val processor = PublishProcessor.create<String>()//(2)
+    processor.//(3)
+    subscribe {
         println("Subscription 1: $it")
-        runBlocking { delay(1000) }
+        runBlocking { delay(2000) }
         println("Subscription 1 delay")
     }
-    connectableFlowable
+    processor//(4)
         .subscribe { println("Subscription 2 $it") }
-    connectableFlowable.connect()
+    flowable.subscribe(processor)//(5)
 }
