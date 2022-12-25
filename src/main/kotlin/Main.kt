225bbc686cf9e8853149f7433824c68614308773
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 22:51:14 +0530

    as Flowable.interval() is emitting one each 100 milliseconds and buffer is gathering emissions within a second timeframe (1 second = 1000 milliseconds, emission with a 100 milliseconds interval would result in 10 emissions in one second).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 import io.reactivex.Flowable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val flowable = Flowable.range(1,111)//(1)
-    flowable.buffer(10,15)//(2)
+    val flowable = Flowable.interval(100, TimeUnit.MILLISECONDS)//(1)
+    flowable.buffer(1, TimeUnit.SECONDS)//(2)
         .subscribe { println(it) }
+    runBlocking { delay(5000) }//(3)
 }
