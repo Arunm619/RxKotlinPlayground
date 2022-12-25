dbbd379dad3e112ded5bd25509453c47ef9c20f8
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:35:10 +0530

    On comment (1), we used the first operator, with the defaultValue parameter set to 2 so that it will emit the defaultValue parameter if it can't access the first element. On comment (2), we used the last operator. On comment (3), we used the first operator again, this time, with an empty Observable; so, instead of emitting the first element, it emits defaultValue.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
 
-fun main() {
-    Observable.range(1, 20)//(1)
-        .filter {//(2)
-            it % 2 == 0
-        }.subscribe {
-            println("Received $it")
-        }
+fun main(args: Array<String>) {
+    val observable = Observable.range(1,10)
+    observable.first(2)//(1)
+        .subscribeBy { item -> println("Received $item") }
+    observable.last(2)//(2)
+        .subscribeBy { item -> println("Received $item") }
+    Observable.empty<Int>().first(2)//(3)
+        .subscribeBy { item -> println("Received $item") }
 }
