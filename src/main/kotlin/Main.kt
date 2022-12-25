f52ad66843d385908490d4c526bcdc93e4bc3609
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:06:11 +0530

    he output clearly shows that the merge operator took emissions from observable2 first, as they came first, even though we put observable1 first in the merge operator. The merge operator, however, supports up to four parameters. As a fallback, we have the mergeArray operator, which accepts vararg of Observable;

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,19 @@
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val observable1 = Observable.interval(500,
-        TimeUnit.MILLISECONDS).map { "Observable 1 $it" }//(1)
-    val observable2 = Observable.interval(100,
-        TimeUnit.MILLISECONDS).map { "Observable 2 $it" }//(2)
-    Observable
-        .merge(observable1,observable2)
+    val observable1 = listOf("A", "B", "C").toObservable()
+    val observable2 = listOf("D", "E", "F", "G").toObservable()
+    val observable3 = listOf("I", "J", "K", "L").toObservable()
+    val observable4 = listOf("M", "N", "O", "P").toObservable()
+    val observable5 = listOf("Q", "R", "S", "T").toObservable()
+    val observable6 = listOf("U", "V", "W", "X").toObservable()
+    val observable7 = listOf("Y", "Z").toObservable()
+    Observable.mergeArray(observable1, observable2, observable3,
+        observable4, observable5, observable6, observable7)
         .subscribe {
             println("Received $it")
         }
-    runBlocking { delay(1500) }
 }
 
 /**
