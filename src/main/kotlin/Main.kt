dbc69bfc5cd9d66325a6329fb063aaaf9bdc044c
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:13:02 +0530

    Ambiguously combining producers
    The ambiguous combination of producers is probably the easiest among all combination
    types. Think of a situation where you're fetching data from two data sources (may be two
    separate APIs or database tables), and want to proceed with the first one you got and
    discard the other one. In the imperative programming technique, you would probably be
    required to write checks for that; however, with RxKotlin, the amb operator is there to hold
    your back.
    
    The amb operator takes a list of Observable (Iterable<Observable> instance) as
    parameter, subscribes to all Observables present in the Iterable instance, emits the
    items that it got from the first Observable it got an emit from, and discards the rest
    of Observables present on the Iterable instance.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,15 +4,12 @@ import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val observable1 = Observable.interval(500, TimeUnit.MILLISECONDS)
-        .take(2)//(1)
-        .map { "Observable 1 $it" }//(2)
-    val observable2 = Observable.interval(
-        100,
-        TimeUnit.MILLISECONDS
-    ).map { "Observable 2 $it" }//(3)
+    val observable1 = Observable.interval(500,
+        TimeUnit.MILLISECONDS).map { "Observable 1 $it" }//(1)
+    val observable2 = Observable.interval(100,
+        TimeUnit.MILLISECONDS).map { "Observable 2 $it" }//(2)
     Observable
-        .concat(observable1, observable2)
+        .amb(listOf(observable1,observable2))//(3)
         .subscribe {
             println("Received $it")
         }
@@ -20,16 +17,9 @@ fun main(args: Array<String>) {
 }
 
 /**
-the concat operator subscribes to the next source Observable
-in the queue only after it got onComplete from its current source Observable; we also
-know that the Observable instances created with Observable.interval never emit
-onComplete. Rather, they keep emitting numbers until Long.MAX_VALUE is reached. So, as
-a quick fix, we used the take operator on comment (1), which will take the first two
-emissions from Observable.interval and then will append an onComplete notification
-to it so that the concat operator can start listening to the next source Observable as well.
- */
-
-/**
- * Just like the merge operator, the concat operator also has concatArray and concatWith
- * variants, and they work in almost the same way, just concatenating instead of merging.
+ * We can see from the output that the amb operator took the emissions from observable2
+ * and didn't care about observable1, as the observable2 instance emitted first.
+ * Just like other combination operators, amb also has ambArray and ambWith operator
+ * variants.
+ *
  * */
