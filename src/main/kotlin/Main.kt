0f1029ab67410df941093b2e68b70fee8373fb7b
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:25:47 +0530

    The switchIfEmpty operator
    This operator is similar to the defaultIfEmpty operator; the only difference is that, for the
    defaultIfEmpty operator, it adds an emission to empty producers, but for the
    switchIfEmpty operator, it starts emitting from the specified alternative producer if the
    source producer is empty

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import io.reactivex.Observable
 fun main(args: Array<String>) {
     Observable.range(0, 10)//(1)
         .filter { it > 15 }//(2)
-        .defaultIfEmpty(15)//(3)
+        .switchIfEmpty(Observable.range(11, 10))//(3)
         .subscribe {
             println("Received $it")
         }
