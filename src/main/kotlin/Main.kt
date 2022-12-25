b2ed1234dcad335204a3c6470c7651be308d5865
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:23:57 +0530

    The filtering/suppressing operators
    debounce
    distinct and distinctUntilChanged
    elementAt
    Filter
    first and last
    ignoreElements
    skip, skipLast, skipUntil, and skipWhile
    take, takeLast, takeUntil, and takeWhile

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,46 @@
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    val flowable = Flowable.interval(1000, TimeUnit.MILLISECONDS)//(1)
-        flowable.map { it }
-        .subscribe { println(it) }
-    runBlocking { delay(10000) }
+    createObservable()//(1)
+        .debounce(200, TimeUnit.MILLISECONDS)//(2)
+        .subscribe {
+            println(it)//(3)
+        }
 }
-
+fun createObservable(): Observable<String> =
+    Observable.create {
+        it.onNext("R")//(4)
+        runBlocking { delay(100) }//(5)
+        it.onNext("Re")
+        it.onNext("Reac")
+        runBlocking { delay(130) }
+        it.onNext("Reactiv")
+        runBlocking { delay(140) }
+        it.onNext("Reactive")
+        runBlocking { delay(250) }//(6)
+        it.onNext("Reactive P")
+        runBlocking { delay(130) }
+        it.onNext("Reactive Pro")
+        runBlocking { delay(100) }
+        it.onNext("Reactive Progra")
+        runBlocking { delay(100) }
+        it.onNext("Reactive Programming")
+        runBlocking { delay(300) }
+        it.onNext("Reactive Programming in")
+        runBlocking { delay(100) }
+        it.onNext("Reactive Programming in Ko")
+        runBlocking { delay(150) }
+        it.onNext("Reactive Programming in Kotlin")
+        runBlocking { delay(250) }
+        it.onComplete()
+    }
 
 /*
-Operators help us leverage and express business logic and behaviors. There are a lot of
-operators available with RxKotlin. Throughout this book, we will be covering various types
-of operators comprehensively so that you know when to use which operator.
-Remember, to implement business logic and behavior in your applications, you should use
-operators instead of writing blocking code or mixing imperative programming with
-reactive programming.
 
-By keeping algorithms and processes purely reactive, you can easily
-- leverage lower memory usage,
-- flexible concurrency,
-- disposability, which are reduced
-or not achieved if you mix reactive programming with imperative programming*/
+Observer receives only three emits, after which the Observable took at least 200
+milliseconds before emitting the next one.
+
+ */
