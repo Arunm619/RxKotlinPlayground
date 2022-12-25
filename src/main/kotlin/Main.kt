ff4a8de4e24e0cd85e9b0efacc5ea12a28cd9550
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:10:44 +0530

    Asynchronous Data Operators and Transformations
    the map operator, it listens to the upstream producer, performs some operations on their
    emissions, and then emits those modified items to the downstream.
    
    - 5 types
    Filtering/suppressing operators
    Transforming operators
    Reducing operators
    Collection operators
    Error handling operators
    Utility operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,23 @@ import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    val flowable = Flowable.interval(100, TimeUnit.MILLISECONDS)//(1)
-    flowable.throttleFirst(200, TimeUnit.MILLISECONDS)//(2)
+    val flowable = Flowable.interval(1000, TimeUnit.MILLISECONDS)//(1)
+        flowable.map { it }
         .subscribe { println(it) }
     runBlocking { delay(10000) }
 }
+
+
+/*
+Operators help us leverage and express business logic and behaviors. There are a lot of
+operators available with RxKotlin. Throughout this book, we will be covering various types
+of operators comprehensively so that you know when to use which operator.
+Remember, to implement business logic and behavior in your applications, you should use
+operators instead of writing blocking code or mixing imperative programming with
+reactive programming.
+
+By keeping algorithms and processes purely reactive, you can easily
+- leverage lower memory usage,
+- flexible concurrency,
+- disposability, which are reduced
+or not achieved if you mix reactive programming with imperative programming*/
