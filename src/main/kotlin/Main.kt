38767dd8dec1a9cf7f4f7db35908d76e9744ac34
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:51:12 +0530

    The transforming operators
    map
    flatMap, concatMap, and flatMapIterable
    switchMap
    switchIfEmpty
    scan
    groupBy
    startWith
    defaultIfEmpty
    sorted
    buffer
    window
    cast
    delay
    repeat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
-import io.reactivex.Observable
+import io.reactivex.rxkotlin.toObservable
 
 fun main(args: Array<String>) {
-    val observable = Observable.range(1,10)
-    observable
-        .ignoreElements()
-        .subscribe { println("Completed") }//(1)
+    val observable = listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1).toObservable()
+    observable.map {//(1)
+            number ->
+        "Transforming Int to String $number"
+    }.subscribe { item ->
+        println("Received $item")
+    }
 }
+
+/*
+We have already seen a little use of the map operator. For a given
+Observable<T> or Flowable<T>, the map operator will transform an emitted item of type
+T into an emission of type R by applying the provided lambda of Function<T,R> to it.
+*/
