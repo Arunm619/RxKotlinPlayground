af8c3c0e6b3e70f1d315503843170ab732a78a6a
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:03:03 +0530

    The window() operator works almost the same, except that, instead of buffering items in a Collection object, it buffers items in another producer.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 import io.reactivex.Flowable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
 
-fun main(args: Array<String>) {
-    val boundaryFlowable = Flowable.interval(350, TimeUnit.MILLISECONDS)
-    val flowable = Flowable.interval(100, TimeUnit.MILLISECONDS)//(1)
-    flowable.buffer(boundaryFlowable)//(2)
-        .subscribe { println(it) }
-    runBlocking { delay(5*1000) }//(3)
+fun main() {
+    val flowable = Flowable.range(1, 111)//(1)
+    flowable.window(10)
+        .subscribe { flo ->
+            flo.subscribe {//(2)
+                print("$it, ")
+            }
+            println()
+        }
 }
