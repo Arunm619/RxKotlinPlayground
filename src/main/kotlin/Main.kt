765d6f3900b83381bfe050ef5584153dfb195ee3
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:22:54 +0530

    The defaultIfEmpty operator
    although Observable doesn't contain any number above
    10, defaultIfEmpty adds 15 to the Observable as it's empty after filtering

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,10 @@
 import io.reactivex.Observable
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.rxkotlin.toObservable
 
-fun main() {
-    val observable = listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1).toObservable()
-    observable.flatMap { number ->
-        Observable.create<String> {//(1)
-            it.onNext("The Number $number")
-            it.onNext("number/2 ${number / 2}")
-            it.onNext("number%2 ${number % 2}")
-            it.onComplete()//(2)
+fun main(args: Array<String>) {
+    Observable.range(0, 10)//(1)
+        .filter { it > 15 }//(2)
+        .defaultIfEmpty(15)//(3)
+        .subscribe {
+            println("Received $it")
         }
-    }.subscribeBy(onNext = { item ->
-        println("Received $item")
-    }, onComplete = {
-        println("Complete")
-    })
 }
