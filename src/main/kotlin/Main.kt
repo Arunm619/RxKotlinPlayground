2644f23fe43ed3799536b7a7d4a01bd711642fa9
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 21:16:35 +0530

    The request() method will request the number of emissions the Subscriber should listen on from the upstream, counting after the method is called. The Subscriber will ignore any further emissions after the requested emissions until you request for more.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,35 @@ import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 
 fun main() {
     Flowable.range(1, 1000)//(1)
-        .map { MyItem4(it) }//(2)
-        .observeOn(Schedulers.io()).subscribe({//(3)
-            println("Received $it")
-            runBlocking { delay(50) }//(4)
-        }, { it.printStackTrace() })
-    runBlocking { delay(60000) }//(5)
+        .map { MyItem5(it) }//(2)
+        .observeOn(Schedulers.io()).subscribe(object : Subscriber<MyItem5> {
+            //(3)
+            override fun onSubscribe(subscription: Subscription) {
+                subscription.request(Long.MAX_VALUE)//(4)
+            }
+
+            override fun onNext(s: MyItem5?) {
+                runBlocking { delay(50) }
+                println("Subscriber received " + s!!)
+            }
+
+            override fun onError(e: Throwable) {
+                e.printStackTrace()
+            }
+
+            override fun onComplete() {
+                println("Done!")
+            }
+        })
+    runBlocking { delay(60000) }
 }
 
-data class MyItem4(val id: Int) {
+data class MyItem5(val id: Int) {
     init {
         println("MyItem Created $id")
     }
