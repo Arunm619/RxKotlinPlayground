4ab0267c7e5513b99dcc25ae62d1140817756d67
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:47:12 +0530

    The collection operators
    toList and toSortedList
    toMap
    toMultiMap
    collect

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.subscribeBy
 
-fun main(args: Array<String>) {
+fun main() {
     Observable.range(1,10)
-        .all { it !is Int }
-        .subscribeBy { println("accumulation $it") }
+        .toList()
+        .subscribeBy { println(" $it") }
 }
+/**
+ * Though it is not good practice, keeping some rare situations in mind, RxKotlin provides
+ * you with operators that can listen to all the emissions and accumulate them to a collection
+ * object.
+ * The collection operators are basically a subset of the reducing operators.
+ * Covered in later chapters.
+ * */
