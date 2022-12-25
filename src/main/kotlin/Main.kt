8c6d9c502006564e960329bb6cd8a79d43f7a9a2
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:28:22 +0530

    The startWith operator
    The startWith operator is simple; it enables you to add an item to the producer at the top
    of all preexisting items.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.toObservable
 
-fun main(args: Array<String>) {
-    Observable.range(0, 10)//(1)
-        .filter { it > 15 }//(2)
-        .switchIfEmpty(Observable.range(11, 10))//(3)
+fun main() {
+    Observable.range(0,10)//(1)
+        .startWith(-1)//(2)
+        .subscribe {
+            println("Received $it")
+        }
+    listOf("C","C++","Java","Kotlin","Scala","Groovy")//(3)
+        .toObservable()
+        .startWith("Programming Languages")//(4)
         .subscribe {
             println("Received $it")
         }
