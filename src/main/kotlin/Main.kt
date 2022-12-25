f85c46ca54c6bb7f30959c2d2a27a17dfb6dce00
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:45:16 +0530

    any operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,6 @@ import io.reactivex.rxkotlin.subscribeBy
 
 fun main(args: Array<String>) {
     Observable.range(1,10)
-        .reduce { previousAccumulation, newEmission ->
-            previousAccumulation+newEmission }
-        .subscribeBy { println("accumulation $it") }
-    Observable.range(1,5)
-        .reduce { previousAccumulation, newEmission ->
-            previousAccumulation*10+newEmission }
+        .any { it !is Int }
         .subscribeBy { println("accumulation $it") }
 }
-/**
- * The reduce operator works similar to the scan operator, the only difference is that instead
- * of accumulating and emitting them on each emission, it accumulates all the emissions and
- * emits them on receiving the onComplete notification.
- * */
