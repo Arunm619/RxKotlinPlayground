6988176e68f994e88ccd19ce1508f1e210126396
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:09:11 +0530

    Concatenating producers (Observable/Flowable)
    Concatenating operators are almost the same with merge operators, except that the
    concatenating operators respect the prescribed ordering. Instead of subscribing to all
    provided producers in one go, it subscribes to the producers one after another; only once, it
    received onComplete from the previous subscription

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,35 @@
-import io.reactivex.rxkotlin.toObservable
+import io.reactivex.Observable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val observable1 = listOf("Kotlin", "Scala",
-        "Groovy").toObservable()
-    val observable2 = listOf("Python", "Java", "C++",
-        "C").toObservable()
-    observable1
-        .mergeWith(observable2)
+    val observable1 = Observable.interval(500, TimeUnit.MILLISECONDS)
+        .take(2)//(1)
+        .map { "Observable 1 $it" }//(2)
+    val observable2 = Observable.interval(
+        100,
+        TimeUnit.MILLISECONDS
+    ).map { "Observable 2 $it" }//(3)
+    Observable
+        .concat(observable1, observable2)
         .subscribe {
             println("Received $it")
         }
+    runBlocking { delay(1500) }
 }
+
+/**
+the concat operator subscribes to the next source Observable
+in the queue only after it got onComplete from its current source Observable; we also
+know that the Observable instances created with Observable.interval never emit
+onComplete. Rather, they keep emitting numbers until Long.MAX_VALUE is reached. So, as
+a quick fix, we used the take operator on comment (1), which will take the first two
+emissions from Observable.interval and then will append an onComplete notification
+to it so that the concat operator can start listening to the next source Observable as well.
+ */
+
+/**
+ * Just like the merge operator, the concat operator also has concatArray and concatWith
+ * variants, and they work in almost the same way, just concatenating instead of merging.
+ * */
