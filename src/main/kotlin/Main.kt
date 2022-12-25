31eb46b53ebf8a5ba66ef4bf4b656d626d9547dc
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 01:46:49 +0530

    Combining producers (Observable/Flowable)
    startWith()
    merge(), mergeDelayError()
    concat()
    zip()
    combineLatest()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,28 @@
 import io.reactivex.Observable
-import io.reactivex.rxkotlin.subscribeBy
 
 fun main(args: Array<String>) {
-    Observable.just(1, 2, 3, 5, 6, 7, 8, 9, 10)
-        .map { it.toIntOrError() }
-        .doOnNext {
-            println("onNext called. 1")
+    println("startWith Iterator")
+    Observable.range(5,10)
+        .startWith(listOf(1,2,3,4))//(1)
+        .subscribe {
+            println("Received $it")
         }
-        .doOnNext {
-        println("onNext called. 2")
-    }
-        .doOnNext {
-            println("onNext called. 3")
+    println("startWith another source Producer")
+    Observable.range(5,10)
+        .startWith(Observable.just(1,2,3,4))//(2)
+        .subscribe {
+            println("Received $it")
         }
-        .doOnNext {
-            println("onNext called. 4")
-        }
-        .doOnComplete {
-            println("onComplete called.")
-        }
-        .doOnSubscribe {
-            println("onSubscribe called.")
-        }
-        .subscribeBy(onNext = {
-        println("Next $it")
-    }, onError = {
-        println("Error $it")
-    }).dispose()
-}
-
-fun Any?.toIntOrError(): Int {
-    if (this is Int) return this else throw Exception("WTF bro, pass an int.")
 }
 
 /**
-These operators help us to perform various utility operations, such as performing some
-action on emissions, remembering timestamps of each items emitted, caching, and much
-more.
-The following is the list of utility operators:
-doOnNext, doOnComplete, and doOnError
-doOnSubscribe, doOnDispose, and doOnSuccess
-serialize
-cache
-
- */
+We can pass another source Observable or an Iterator instance to be prepended before
+the source Observable that the operator has subscribed to starts emitting.
+In the preceding program, on comment (1), we used the startWith operator and passed
+an Interator instance to it. The startWith operator internally converts the passed
+Iterator instance to an Observable instance (it'll convert it to a Flowable instance in
+case you're using Flowable). Here is the signature of the startWith operator:
+fun startWith(items: Iterable<T>): Observable<T> {
+return concatArray<T>(fromIterable<out T>(items), this)
+}
+ * */
