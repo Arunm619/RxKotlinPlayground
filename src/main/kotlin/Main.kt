2c6d6f664f0744aaaf8ef6ad6e01c4c69808800a
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-25 23:33:27 +0530

    we requested the 5 th element from Observable, and it emitted the same (count starts with zero). However, on comment (2), we requested the 50th element, which doesn't even exist in Observable, so it didn't emit anything. This operator achieves this behavior with the help of the Maybe monad, which will be covered later.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import io.reactivex.rxkotlin.toObservable
 
 fun main(args: Array<String>) {
-    listOf(1,2,2,3,4,5,5,5,6,7,8,9,3,10)//(1)
-        .toObservable()//(2)
-        .distinctUntilChanged()//(3)
-        .subscribe { println("Received $it") }//(4)
+    val observable = listOf(10,1,2,5,8,6,9)
+        .toObservable()
+    observable.elementAt(5)//(1)
+        .subscribe { println("Received $it") }
+    observable.elementAt(50)//(2)
+        .subscribe { println("Received $it") }
 }
