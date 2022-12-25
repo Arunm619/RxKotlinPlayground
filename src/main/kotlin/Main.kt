90d5becb9e4bfd4112df9945cc3d93687f54caae
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 02:56:35 +0530

    SwitchMap
    When the source Observable emits more than one item consecutively before the
     switchMap has emitted any of them, switchMap will take the last one and discard any emission that came in between

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,68 +3,31 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    concatMapExample()
-    flatMapExample()
-}
-
-/**
-* Uses concat internally.
-* */
-fun concatMapExample() {
+    println("Without delay")
     Observable.range(1,10)
-        .concatMap {
-            val randDelay = Random().nextInt(1000)
-            return@concatMap Observable.just(it)
-                .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)//(1)
+        .switchMap {
+            return@switchMap Observable.just(it)//(1)
         }
         .blockingSubscribe {
             println("Received $it")
         }
-}
-
-/**
- * As the concatMap operator uses concat internally, it maintains the prescribed order of
- * emissions.
- * */
-
-/**
- * Uses Merge internally
- * */
-fun flatMapExample() {
-    Observable.range(1,10)
-        .flatMap {
+    println("With delay")
+    Observable.range(1,15)
+        .switchMap {
             val randDelay = Random().nextInt(10)
-            return@flatMap Observable.just(it)
-                .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)//(1)
+            return@switchMap Observable.just(it)
+                .also {
+                    //println("The value is ${it.blockingSingle()}")
+                }
+                .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)//(2)
         }
         .blockingSubscribe {
             println("Received $it")
         }
 }
-/*
-*  the cause
-behind it is simply the merge operator, as the merge operator subscribes and reemits the
-emissions asynchronously all at one go, thus the order is not maintained.
-* */
-
 
 /**
- * When to use flatMap operator
- * Take a look at the following list—it contains the contexts and situations where flatMap will
- * fit best:
- * When you're working with a list of data within a page, activity, or fragment and
- * want to send some data to a server or a database per item of the list. The
- * concatMap operator will also do here; however, as the flatMap operator works
- * asynchronously, it'll be faster, and, as you're sending data, the order doesn't
- * really matter.
- * Whenever you want to perform any operation on list items asynchronously and
- * in a comparatively short time period.
- * When to use concatMap operator
- * So, when to use concatMap?
- * The following list contains the contexts and situations where concatMap will fit best:
- * When you are downloading the list of data to display to the user. The order really
- * matters here, you will surely not want to load and display the second item of the
- * list after the third and fourth one are already displayed, would you?
- * Performing some operation on a sorted list, making sure the list stays the same
- *
+ * When the source Observable emits more than one item consecutively before the
+ * switchMap has emitted any of them, switchMap will take the last one and discard any
+ * emission that came in between
  * */
