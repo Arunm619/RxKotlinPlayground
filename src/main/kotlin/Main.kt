422287a35ac067d4c84c5908da066751c24066ba
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 00:21:39 +0530

    The flatMap operator
    Where the map operator takes each emission and transforms them, the flatMap operator
    creates a new producer, applying the function you passed to each emission of the source
    producer.
    
    The output is similar to the previous one, but the logic is different. Instead of just returning
    the String, we are returning Observable with the desired String. Although, for this
    example, you seem to have no benefit using it, think of a situation when you need to derive
    multiple items from a single emission.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.toObservable
 
-fun main(args: Array<String>) {
+fun main() {
     val observable = listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1).toObservable()
     observable.flatMap { number ->
-        Observable.just("Transforming Int to String $number")
-    }.subscribe { item ->
+        Observable.create<String> {//(1)
+            it.onNext("The Number $number")
+            it.onNext("number/2 ${number / 2}")
+            it.onNext("number%2 ${number % 2}")
+            it.onComplete()//(2)
+        }
+    }.subscribeBy(onNext = { item ->
         println("Received $item")
-    }
+    }, onComplete = {
+        println("Complete")
+    })
 }
