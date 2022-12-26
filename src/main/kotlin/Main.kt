eafdf6976c4d81fd82cfca2cafabe9d79e1af094
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 23:48:48 +0530

    Schedulers.trampoline()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,8 @@
 /**
- * The Schedulers.newThread() provides us with a scheduler that creates a new thread for
- * each task provided. While at first glance it may seem similar to Schedulers.io(), there's
- * actually a huge difference.
  *
- * The Schedulers.single() provides us with a scheduler that contains only one thread
- * and returns the single instance for every call. Confused? Let's make it clear. Think of a
- * situation where you need to execute tasks that are strongly
- * sequential—Schedulers.single() is the best available option for you here. As it
- * provides you with only one thread, every task that you enqueue here is bound to be
- * executed sequentially
+ * Schedulers.single() and Schedulers.trampoline() sound somewhat similar, both
+ * the schedulers are for sequential execution. While Schedulers.single() guarantees that
+ * all its task will run sequentially, it may run parallel to the thread it was called upon (if not,
+ * that thread is from Schedulers.single() as well); the Schedulers.trampoline() is
+ * different in that sector.
  * */
