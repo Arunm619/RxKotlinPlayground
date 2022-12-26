3ef7a9ec6706759f02e961278f8f3a5d20cfe9bc
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 18:09:39 +0530

    The onErrorResumeNext operator
    The onErrorResumeNext operator helps you subscribe to a different producer in case any
    error occurs.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,15 @@
 import io.reactivex.Observable
 
 fun main() {
-    Observable
-        .just(1, 2, 3, 4, 5)
-        .map { it / (3 - it) }
-        .onErrorReturn { -1 }//(1)
+    Observable.just(1,2,3,4,5)
+        .map { it/(3-it) }
+        .onErrorResumeNext(Observable.range(10,5))//(1)
         .subscribe {
             println("Received $it")
         }
 }
 
 /**
- *
- * The onErrorReturn operator returns the specified default
- * value. The downstream didn't receive any item further as the upstream stopped emitting
- * items as soon as the error occurred.
- *
- * (i) both onError and onComplete are terminal
- * operators, so the downstream stops listening to that upstream as soon as it
- * receives any of them.
+ * This operator is especially useful when you want to subscribe to another source producer in
+ * case any error occurs.
  * */
