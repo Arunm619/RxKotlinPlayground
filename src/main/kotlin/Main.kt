b715c81dace4ef75f5377a57cc00db9111c344dd
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 01:28:43 +0530

    Composing operators with transformer Transformers - Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,32 @@
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
-import io.reactivex.annotations.NonNull
+import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
-/**
- * RxKotlin provides the Transformer interfaces (ObservableTransformer and
- * FlowableTransformer are two Transformer interfaces) for that purpose. Just like the
- * operator interfaces, it has only one method—apply. The only difference is that here,
- * instead of Observers, you have the Observable. So, instead of operating on individual
- * emits and their items, here, you work directly on the source.
- * */
-
-interface ObservableTransformer<Upstream, Downstream> {
-    /**
-     * Applies a function to the upstream Observable
-    and returns an ObservableSource with
-     * optionally different element type.
-     * @param upstream the upstream Observable instance
-     * @return the transformed ObservableSource instance
-     */
-    @NonNull
-    fun apply(@NonNull upstream: Observable<Upstream>):
-            ObservableSource<Downstream>
+fun main(args: Array<String>) {
+    Observable.range(1, 10).map {
+        println("map - ${Thread.currentThread().name} $it")
+        it
+    }.compose(
+            SchedulerManager(
+                Schedulers.computation(), Schedulers.io()
+            )
+        ).subscribe {
+            println("onNext - ${Thread.currentThread().name} $it")
+        }
+    runBlocking { delay(100) }
 }
 
-/**
- * The interface signature is almost the same. Unlike the apply method of
- * ObservableOperator, here, the apply method receives Upstream Observable and
- * should return the Observable that should be passed to the Downstream.
- * */
+class SchedulerManager<T>(
+    private val subscribeScheduler: Scheduler,
+    private val observeScheduler: Scheduler
+) : ObservableTransformer<T, T> {
+    override fun apply(upstream: Observable<T>): ObservableSource<T> {
+        return upstream
+            .subscribeOn(subscribeScheduler)
+            .observeOn(observeScheduler)
+    }
+}
