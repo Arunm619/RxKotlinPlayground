3d4d3d3ef209047642916c73718c45e2533d8184
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 18:11:39 +0530

    Retrying on error

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,33 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
 
-fun main() {
+fun main(args: Array<String>) {
     Observable.just(1,2,3,4,5)
         .map { it/(3-it) }
-        .onErrorResumeNext(Observable.range(10,5))//(1)
-        .subscribe {
-            println("Received $it")
+        .retry(3)//(1)
+        .subscribeBy (
+            onNext = {println("Received $it")},
+            onError = {println("Error")}
+        )
+    println("\n With Predicate \n")
+    var retryCount = 0
+    Observable.just(1,2,3,4,5)
+        .map { it/(3-it) }
+        .retry {//(2)
+                _, _->
+            (++retryCount)<3
         }
+        .subscribeBy (
+            onNext = {println("Received $it")},
+            onError = {println("Error")}
+        )
 }
-
 /**
- * This operator is especially useful when you want to subscribe to another source producer in
- * case any error occurs.
+ * The retry operator is another error handling operator that enables you to retry/resubscribe to the same producer when an error occurs. You just need to provide a predicate
+ * or retry-limit when it should stop retrying.
+ *
+ * we used the retry operator with a retry limit, and on comment (2), we
+ * used the retry operator with a predicate. The retry operator will keep retrying until the
+ * predicate returns true and will pass the error to downstream whenever the predicate
+ * returns false.
  * */
