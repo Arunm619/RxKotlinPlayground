17833bd6d54d4d50a9e9e4e1d694dbdd84581432
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 00:27:55 +0530

    Testing in RxKotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,10 @@
 /**
- * TDD
- * Test-driven development is a development methodology where tests are
- * written first, and then the actual source code is written that would pass the
- * test cases. Test-driven development is hugely popular among developers
- * and architects and many companies follow TDD as their development
- * process.
+ * Now, as you've some hands-on testing in Kotlin and have some idea about RxKotlin as
+ * well, you may be wondering how to implement test cases in RxKotlin? It is true that testing
+ * in RxKotlin may not seem straightforward; the reason is that ReactiveX defines behavior
+ * rather than states, and most testing frameworks, including JUnit and kotlin—test are good
+ * for testing states.
+ * To the aid of developers, RxKotlin comes with a set of tools for testing, which you can use
+ * with your favorite testing frameworks. In this book, we will cover testing in RxKotlin with
+ * JUnit and Kotlin-test.
  * */
-
-fun add(a:Int, b:Int):Int = a+b
-fun substract(a:Int, b:Int):Int = a-b
-fun mult(a:Int, b:Int):Int = a*b
-fun divide(a:Int, b:Int):Int = a/b
