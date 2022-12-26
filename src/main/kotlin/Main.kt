6ec3ce515746d6346d6273ab2109083053726f1a
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 23:51:37 +0530

    Schedulers.from
    So far, we've seen the default/predefined schedulers available within RxKotlin. However,
    while developing applications, you may need to define your custom scheduler. Keeping
    that scenario in mind, ReactiveX has provided you with
    Schedulers.from(executor:Executor), which lets you convert any executor into a
    scheduler

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,42 @@
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+fun main() {
+    val executor: Executor = Executors.newFixedThreadPool(2)//(1)
+    val scheduler: Scheduler = Schedulers.from(executor)//(2)
+    Observable.range(1, 10).subscribeOn(scheduler)//(3)
+        .subscribe {
+            runBlocking { delay(200) }
+            println("Observable1 Item Received $it -${Thread.currentThread().name}")
+        }
+    Observable.range(21, 10).subscribeOn(scheduler)//(4)
+        .subscribe {
+            runBlocking { delay(100) }
+            println(
+                "Observable2 Item Received $it -${Thread.currentThread().name}"
+            )
+        }
+    Observable.range(51, 10).subscribeOn(scheduler)//(5)
+        .subscribe {
+            runBlocking { delay(100) }
+            println(
+                "Observable3 Item Received $it -${Thread.currentThread().name}"
+            )
+        }
+    runBlocking { delay(10000) }//(6)
+}
+
 /**
- *
- * Schedulers.single() and Schedulers.trampoline() sound somewhat similar, both
- * the schedulers are for sequential execution. While Schedulers.single() guarantees that
- * all its task will run sequentially, it may run parallel to the thread it was called upon (if not,
- * that thread is from Schedulers.single() as well); the Schedulers.trampoline() is
- * different in that sector.
+ *  we've created a custom Scheduler from an Executor (for the sake of
+ * simplicity, we've used a standard Thread Pool Executor; you're free to use your own custom
+ * executor).
+ * On comment (1), we created the executor with the Executors.newFixedThreadPool()
+ * method, on comment (2), we created the scheduler instance with the help of
+ * Schedulers.from(executor:Executor). We used the scheduler instance on comment
+ * (3), comment (4), and comment (5).
  * */
