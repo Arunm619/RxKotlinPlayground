9710a5b60c28dc4ac909788461a7174f823ab762
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 17:52:25 +0530

    Skipping and taking emissions
    skip - this.
    skipLast
    skipWhile
    skipUntil

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,57 @@
 import io.reactivex.Observable
-import java.util.*
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    Observable.range(1,10)
-        .switchMap {
-            val randDelay = Random().nextInt(10)
-            if(it%3 == 0)
-                Observable.just(it)
-            else
-                Observable.just(it)
-                    .delay(randDelay.toLong(), TimeUnit.MILLISECONDS)
-        }
-        .blockingSubscribe {
-            println("Received $it")
-        }
+    val observable1 = Observable.range(1, 20)
+    observable1.skip(5)//(1)
+        .subscribe(object : Observer<Int> {
+            override fun onError(e: Throwable) {
+                println("Error $e")
+            }
+
+            override fun onComplete() {
+                println("Complete")
+            }
+
+            override fun onNext(t: Int) {
+                println("Received $t")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                println("starting skip(count)")
+            }
+        })
+    val observable2 = Observable.interval(100, TimeUnit.MILLISECONDS)
+    observable2.skip(400, TimeUnit.MILLISECONDS)//(2)
+        .subscribe(object : Observer<Long> {
+            override fun onError(e: Throwable) {
+                println("Error $e")
+            }
+
+            override fun onComplete() {
+                println("Complete")
+            }
+
+            override fun onNext(t: Long) {
+                println("Received $t")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                println("starting skip(time)")
+            }
+        })
+    runBlocking {
+        delay(1000)
+    }
 }
 
 /**
- *  instead of adding delay to all the emissions, we emitted all the numbers
- * divisible by 3 without delay, and added a delay to the rest.
+ * The skip operator has two important overloads: skip(count:Long) and
+ * skip(time:Long, unit:TimeUnit); the first overload works on count, discarding the
+ * first n number of emissions, while the second overload works on time, discarding all the
+ * emissions that came in the specified time duration.
  * */
-
-/**
- * When the source Observable emits more than one item consecutively before the
- * switchMap has emitted any of them, switchMap will take the last one and discard any
- * emission that came in between
- * */
-
-// Check this: https://stackoverflow.com/a/38391535
