52f61b76a007134659c5cf0bd947f9fe2cb8e22d
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 00:40:49 +0530

    Blocking operators Getting the only item from single or maybe - blockingGet

--- src/test/kotlin/TestClass.kt
+++ src/test/kotlin/TestClass.kt
@@ -1,4 +1,6 @@
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.rxkotlin.toObservable
 import io.reactivex.schedulers.Schedulers
 import org.junit.jupiter.api.Test
@@ -7,17 +9,25 @@ import kotlin.test.assertEquals
 
 class TestClass {
     @Test
-    fun `test with blockingFirst`() {
+    fun `test Single with blockingGet`() {
         val observable = listOf(2,10,5,6,9,8,7,1,4,3).toObservable()
             .sorted()
-        val firstItem = observable.blockingFirst()
+        val firstElement: Single<Int> = observable.first(0)
+        val firstItem = firstElement.blockingGet()
         assertEquals(1,firstItem)
     }
-
+    @Test
+    fun `test Maybe with blockingGet`() {
+        val observable = listOf(2,10,5,6,9,8,7,1,4,3).toObservable()
+            .sorted()
+        val firstElement: Maybe<Int> = observable.firstElement()
+        val firstItem = firstElement.blockingGet()
+        assertEquals(1,firstItem)
+    }
     /**
-     * we created an unsorted list of integers from 1 to 10 and created an
-     * Observable with the list, so the smallest item from that Observable should be 1. We
-     * obtained the first item and made the thread to wait till we get it with the help of
-     * the blockingFirst() operator.
+     * In the first test case, we used observable.first() with a default value, this operator
+     * returns a Single; on the second operator, we used observable.firstElement() this
+     * operator returns a Maybe. Then we used blockingGet in both test cases to get the first
+     * element as an Int and execute the test function.
      * */
 }
