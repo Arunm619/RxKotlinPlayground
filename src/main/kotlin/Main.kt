f20725d58223ae37ec735463d43407efe2e172c9
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 00:37:48 +0530

    Blocking operators Getting the first emitted item – blockingFirst()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,3 +11,27 @@ fun main(args: Array<String>) {
         }
     runBlocking { delay(1) }
 }
+
+/**
+ * While blockingSubscribe is useful in testing, it cannot always serve your purpose. You
+ * might need to test the first, last or all the values of the producer. For that purpose you
+ * would need the data in its pure imperative nature.
+ * The set of yet uncovered operators in RxKotlin is at your helm in that scenario. The
+ * blocking operators serve as an immediate accessible bridge between the reactive world and
+ * the imperative world. They block the current thread and make it wait for the results to be
+ * emitted, but returns them in a non-reactive way.
+ * The only similarity between blockingSubscribe and blocking operators are that both
+ * block the declaring thread even if the reactive operations are performed in a different
+ * thread.
+ *
+ *
+ *  The blockingSubscribe treats the data
+ * as reactive and doesn't return anything. It rather pushes them to the subscriber (or lambda)
+ * specified. Whereas blocking operators will return the data in a non-reactive nature.
+ * The following list contains the blocking operators we are going to cover:
+ * blockingFirst()
+ * blockingGet()
+ * blockingLast()
+ * blockingIterable()
+ * blockingForEach()
+ * */
--- src/test/kotlin/TestClass.kt
+++ src/test/kotlin/TestClass.kt
@@ -1,37 +1,23 @@
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.toObservable
 import io.reactivex.schedulers.Schedulers
 import org.junit.jupiter.api.Test
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.test.assertEquals
 
 class TestClass {
-    @Test//(1)@Test
-    fun `check emissions count`() {
-        val emissionsCount = AtomicInteger()//(1)
-        Observable
-            .range(1, 10)
-            .subscribeOn(Schedulers.computation())
-            .blockingSubscribe {//(2)
-                emissionsCount.incrementAndGet()
-            }
-        assertEquals(10, emissionsCount.get())//(3)
+    @Test
+    fun `test with blockingFirst`() {
+        val observable = listOf(2,10,5,6,9,8,7,1,4,3).toObservable()
+            .sorted()
+        val firstItem = observable.blockingFirst()
+        assertEquals(1,firstItem)
     }
 
     /**
-     * AtomicInteger is a wrapper around integer in Java, that allows an Int value to be
-     * updated atomically. Though AtomicInteger extends Number to allow uniform access by
-     * tools and utilities that deal with numerically-based classes, it cannot be used as a
-     * replacement of Integer. We used AtomicInteger in this code to ensure atomicity, as the
-     * subscription was running in the computationScheduler (thus in multiple threads).
-     *
-     *
-     * The line, that demands our attention is where we put comment (2). We used
-     * blockingSubscribe instead of just subscribe. When we subscribe to a producer with
-     * the subscribe operator and the subscription is not in the current thread, the current thread
-     * doesn't wait for the subscription to complete and moves to the next line instantly. That's
-     * why we used delay to make the current thread wait. Using delay inside tests is
-     * troublesome. While blockingSubscribe blocks the current running thread until the
-     * subscription finishes up (even if the subscription occurs in a separate thread), that is useful
-     * while writing tests.
+     * we created an unsorted list of integers from 1 to 10 and created an
+     * Observable with the list, so the smallest item from that Observable should be 1. We
+     * obtained the first item and made the thread to wait till we get it with the help of
+     * the blockingFirst() operator.
      * */
 }
