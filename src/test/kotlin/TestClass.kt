3e2126a8fc4921ec68fed21ef64fba7ff10d7e80
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 00:58:18 +0530

    Understanding TestScheduler
    TestScheduler is here to save your life. They can effectively simulate time with timedriven producers so that we can do assertions by fast-forwarding it by a specific amount

--- src/test/kotlin/TestClass.kt
+++ src/test/kotlin/TestClass.kt
@@ -1,64 +1,35 @@
+import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
-import io.reactivex.rxkotlin.toFlowable
-import io.reactivex.rxkotlin.toObservable
-import io.reactivex.subscribers.TestSubscriber
+import io.reactivex.schedulers.TestScheduler
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 
 class TestClass {
     @Test
-    fun `test with TestObserver`() {
-        val list = listOf(2, 10, 5, 6, 9, 8, 7, 1, 4, 3, 12, 20, 15, 16, 19, 18, 17, 11, 14, 13)
-        val observable = list.toObservable().sorted()
-        val testObserver = TestObserver<Int>()
-        observable.subscribe(testObserver)//(1)
-        testObserver.assertSubscribed()//(2)
-        testObserver.awaitTerminalEvent()//(3)
-        testObserver.assertNoErrors()//(4)
-        testObserver.assertComplete()//(5)
-        testObserver.assertValueCount(20)//(6)
-        testObserver.assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)//(7)
+    fun `test by fast forwarding time`() {
+        val testScheduler = TestScheduler()
+        val observable = Observable.interval(5, TimeUnit.MINUTES, testScheduler)
+        val testObserver = TestObserver<Long>()
+        observable.subscribe(testObserver)
+        testObserver.assertSubscribed()
+        testObserver.assertValueCount(0)//(1)
+        testScheduler.advanceTimeBy(100, TimeUnit.MINUTES)//(2)
+        testObserver.assertValueCount(20)//(3)
+        testScheduler.advanceTimeBy(400, TimeUnit.MINUTES)//(4)
+        testObserver.assertValueCount(100)//(5)
     }
 
-    @Test
-    fun `test with TestSubscriber`() {
-        val list = listOf(2, 10, 5, 6, 9, 8, 7, 1, 4, 3, 12, 20, 15, 16, 19, 18, 17, 11, 14, 13)
-        val flowable = list.toFlowable().sorted()
-        val testSubscriber = TestSubscriber<Int>()
-        flowable.subscribe(testSubscriber)//(1)
-        testSubscriber.assertSubscribed()//(2)
-        testSubscriber.awaitTerminalEvent()//(3)
-        testSubscriber.assertNoErrors()//(4)
-        testSubscriber.assertComplete()//(5)
-        testSubscriber.assertValueCount(20)//(6)
-        testSubscriber.assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)//(7)
-    }
     /**
-     * So, here are your two superheroes to make the developers life easy—TestObserver and
-     * TestSubscriber. As with Subscriber and Observer, you can use TestSubscriber
-     * with Flowables and TestObserver with Observables, everything except that is similar
-     * between these two.
      *
+     * On comment (1), it should not receive any emissions (as there are still 5 minutes before it
+     * should receive its first emission) and we are testing it with assertValuesCount(0).
      *
-     * Let's now understand the test cases.
-     * On comment (1), we are subscribing to the
-     * Observable/Flowable.
+     * We then fast-forwarded the time by 100 minutes on comment (2), and tested whether we
+     * received 20 emissions on comment (3).
+     * TestScheduler provides us with the advanceTimeBy method, which takes a timespan and unit as parameters and simulates that
+     * for us.
      *
-     * On comment (2), we are checking if the Subscription was successful
-     * and was only one with the help of the assertSubscribed() test.
-     *
-     * On comment (3), we are blocking the thread until the Observable/Flowable completes its execution with the
-     * awaitTerminalEvent() method. This terminal event can be onComplete or onError as
-     * well.
-     *
-     * On comments (4) and (5), we are checking whether the Observable and/or
-     * Flowable has completed successfully without any errors, assertNoErrors() will test
-     * whether the Subscription hasn't received any errors and
-     *
-     * assertComplete() will test whether the producer has completed successfully .
-     *
-     * On comment (6), we are testing that the total received emission count was 20 (there were 20 items in the list),
-     * assertValuesCount() helps us with this objective.
-     *
-     * On comment (6), with the help of assertValues() we are testing the expected and actual values of each of the emissions in its order.
+     * We then fast-forwarded time by another 400 minutes and tested if we received a total of 100
+     * emissions on comment (4) and comment (5).
      * */
 }
