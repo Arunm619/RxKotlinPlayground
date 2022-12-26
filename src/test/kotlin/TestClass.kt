8687d1f72f585121014836fbbe81b1a0a3b9dc51
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 00:12:48 +0530

    Writing JUnit tests in Kotlin Failed a test purposefully with some error msg.

--- src/test/kotlin/TestClass.kt
+++ src/test/kotlin/TestClass.kt
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+
 class TestClass {
     @Test//(1)
     fun `my first test`() {//(2)
-        assertEquals(3,2)//(3)
+        assertEquals(
+            3, 2 + 3, "Actual value is not equal to the expected one."
+        )
     }
 }
