3f7b23187f10f100a752beeb83775319a9cda5bc
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 18:03:51 +0530

    Take operators
    take
    takeLast - this
    takeWhile
    takeUntil

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,25 @@
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.TimeUnit
 
 fun main() {
-    val observable1 = Observable.range(1,20)
-    observable1
-        .take(5)//(1)
+    val observable = Observable.range(1,20)
+    observable
+        .takeWhile{item->item<10}//(1)
         .subscribe(object: Observer<Int> {
             override fun onError(e: Throwable) {
                 println("Error $e")
             }
             override fun onComplete() {
-                println("onComplete")
+                println("Complete")
             }
             override fun onNext(t: Int) {
                 println("Received $t")
             }
             override fun onSubscribe(d: Disposable) {
-                println("starting skip(count)")
+                println("starting skipWhile")
             }
         })
-    val observable2 = Observable.interval(100, TimeUnit.MILLISECONDS)
-    observable2
-        .take(400,TimeUnit.MILLISECONDS)//(2)
-        .subscribe(
-            object:Observer<Long> {
-                override fun onError(e: Throwable) {
-                    println("Error $e")
-                }
-                override fun onComplete() {
-                    println("Complete")
-                }
-                override fun onNext(t: Long) {
-                    println("Received $t")
-                }
-                override fun onSubscribe(d: Disposable) {
-                    println("starting skip(time)")
-                }
-            }
-        )
-    runBlocking {
-        delay(1000)
-    }
 }
 
 /**
