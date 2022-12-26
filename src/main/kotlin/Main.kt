89d87eecd34b67c7cbb7cb303a7c010f090aa150
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 19:00:35 +0530

    Concurrency and Parallel Processing in RxKotlin with Schedulers
    Introduction to concurrency
    The subscribeOn() and observeOn() operator
    Parallelization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,9 @@
-import org.apache.http.impl.nio.client.HttpAsyncClients
-import rx.apache.http.ObservableHttp
-
-fun main() {
-    val httpClient = HttpAsyncClients.createDefault()//(1)
-    httpClient.start()//(2)
-    val URL = "https://dummyjson.com/products"
-    ObservableHttp.createGet(
-        URL, httpClient
-    ).toObservable()//(3)
-        .flatMap { response ->
-            response.content.map { bytes ->
-                String(bytes)
-            }//(4)
-        }.onErrorReturn {//(5)
-            "Error Parsing data "
-        }.subscribe {
-            println(it)//(6)
-            httpClient.close()//(7)
-        }
-}
-
 /**
- * In this program, we used HttpAsyncClients.createDefault() to get an instance of
- * CloseableHttpAsyncClient. Before starting an HTTP request, we first need to start the
- * client. We did this in the code on comment (2), with httpClient.start(). On comment
- * (3), we created a GET request and converted it to an observable of type
- * ObservableHttpResponse, so we used the flatMap operator to get access to the content
- * of the response. Inside the flatMap operator, we used the map operator to convert the byte
- * response into a String on comment (4).
+ * A popular misconception regarding reactive programming is that reactive programming is
+ * multi-threaded by default.
+ *
+ * The truth is actually that RxKotlin works on a single thread by
+ * default, although it provides us with loads of operators to implement multi-threading as
+ * per our business logic and requirements with ease.
+ *
  * */
