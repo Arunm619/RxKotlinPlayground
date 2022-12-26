e4d3e7d7d83c77ac1c05a5a7f14120cca3f753c6
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 23:55:23 +0530

    Changing thread on subscription – subscribeOn operator main thread.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,25 @@
+import io.reactivex.rxkotlin.toObservable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+        .toObservable().map { item ->
+            println("Mapping $item ${Thread.currentThread().name}")
+            return@map item.toInt()
+        }
+        .subscribe { item ->
+            println("Received $item${Thread.currentThread().name}")
+        }
+    runBlocking {
+        delay(1000)
+    }
+}
 /**
- * There are basically two operators that help us implement schedulers. Up until now, in this
- * chapter, we've used the subscribeOn operator in all the examples with a scheduler;
- * however, there's another operator—observeOn. We will now focus on these two operators,
- * learning how they work, and how they differ.
+ * From o/p, it's the threads that are responsible for carrying items from
+ * the source all the way to the Subscriber through operators. It may be a single thread
+ * throughout the subscription, or it may even be different threads at different levels.
+ * By default, the thread in which we perform the subscription is the responsible of bringing
+ * all the emissions down to the Subscriber, unless we instruct it otherwise
  * */
