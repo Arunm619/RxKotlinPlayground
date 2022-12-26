1e559bdad8568babc9c43f4ae8948c87a34b5ceb
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-26 18:08:28 +0530

    onErrorReturn – return a default value on error

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ fun main() {
     Observable
         .just(1, 2, 3, 4, 5)
         .map { it / (3 - it) }
+        .onErrorReturn { -1 }//(1)
         .subscribe {
             println("Received $it")
         }
@@ -11,13 +12,11 @@ fun main() {
 
 /**
  *
- * As expected, the program threw an error and that is a bad thing if that occurs on the user
- * end. So, let's take a look at how we can handle errors in a reactive way. RxKotlin provides
- * us with a few operators for error handling, which we'll take a look at. We will use the
- * previous program and apply various error handling operators to them to understand them
- * better
+ * The onErrorReturn operator returns the specified default
+ * value. The downstream didn't receive any item further as the upstream stopped emitting
+ * items as soon as the error occurred.
  *
- *
- * The exception was not handled due to missing onError handler in the subscribe() method call.
- * Always add the onError handler.
+ * (i) both onError and onComplete are terminal
+ * operators, so the downstream stops listening to that upstream as soon as it
+ * receives any of them.
  * */
