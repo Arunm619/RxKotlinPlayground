13a829f451937865e93f2840c395165932021068
Arun sudharsan <arun.sudharsan@meesho.com>
2022-12-27 01:30:29 +0530

    Composing operators with transformer Transformers - Example another example where we will transform an Observable<Int> to an Observable<List>.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,14 @@
 import io.reactivex.Observable
-import io.reactivex.ObservableSource
-import io.reactivex.ObservableTransformer
-import io.reactivex.Scheduler
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import io.reactivex.rxkotlin.subscribeBy
 
 fun main(args: Array<String>) {
-    Observable.range(1, 10).map {
-        println("map - ${Thread.currentThread().name} $it")
-        it
-    }.compose(
-            SchedulerManager(
-                Schedulers.computation(), Schedulers.io()
-            )
-        ).subscribe {
-            println("onNext - ${Thread.currentThread().name} $it")
+    Observable.range(1,10)
+        .compose<List<Int>> {
+                upstream: Observable<Int> ->
+            upstream.toList().toObservable()
         }
-    runBlocking { delay(100) }
-}
-
-class SchedulerManager<T>(
-    private val subscribeScheduler: Scheduler,
-    private val observeScheduler: Scheduler
-) : ObservableTransformer<T, T> {
-    override fun apply(upstream: Observable<T>): ObservableSource<T> {
-        return upstream
-            .subscribeOn(subscribeScheduler)
-            .observeOn(observeScheduler)
-    }
+        .first(listOf())
+        .subscribeBy {
+            println(it)
+        }
 }
